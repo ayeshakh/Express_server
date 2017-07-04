7942fd539a472989721dc5a6941addfb9b47eab7
Ayesha <ayesha_04@yahoo.com>
2017-07-04 19:03:29 +0000

    Template Engine

--- express_server.js
+++ express_server.js
@@ -2,10 +2,18 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs")
+
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
