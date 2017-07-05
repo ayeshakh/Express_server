d8f6053b97ead984785699ccd675b9f37de6f218
Ayesha <ayesha_04@yahoo.com>
2017-07-05 21:00:08 +0000

    cookies

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs")
@@ -19,34 +21,60 @@ function generateRandomString() {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+//app.get("/", (req, res) => {
+  //res.end("Hello!");
+//});
 app.get("/", (req, res) => {
-  res.end("Hello!");
+    //res.end("<html><body>Hello <b>World</b></body></html>\n");
+    res.redirect("/login")
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/", (req, res) => {
+    //res.end("<html><body>Hello <b>World</b></body></html>\n");
+    res.redirect("/login")
 });
 
+app.get("/login", (req, res) => {
+res.render("./partials/_header.ejs", {username: undefined})
+});
+
+app.get("/logout", (req, res) => {
+res.end("<html><body>Logout<b></body></html>\n");
+});
+
+
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
+});
+
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
@@ -72,6 +100,17 @@ app.post("/urls/:id", (req, res) => {
   let longURL = req.body.longURL
   urlDatabase[req.params.id] = longURL;
   res.redirect("/urls")
+  });
+
+app.post("/login", (req, res) => {
+  let username = req.body.username
+  res.cookie('username', username);
+  res.redirect("/urls")
+  });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/login")
   });
 
 
