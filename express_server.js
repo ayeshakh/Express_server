1ca6c723767c55866d826907baebd247ec4b7020
Ayesha <ayesha_04@yahoo.com>
2017-07-05 18:08:43 +0000

    Tiny app Day2 with delete button and edit button added

--- express_server.js
+++ express_server.js
@@ -4,16 +4,25 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+var characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+length = 6 ;
+function generateRandomString() {
+  var stringRandom = "";
+  min = Math.ceil(0);
+  max = Math.floor(characters.length-1);
+  for(i = length; i > 0; i--){
+  stringRandom += characters[Math.floor(Math.random() * (max - min + 1)) + min];
+ }
+ return stringRandom;
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -23,9 +32,52 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+//let shortURL = req.params.shortURL
+  let longURL = urlDatabase[shortURL]
+  res.redirect(longURL);
+});
+
+ app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls")
+  });
+
+app.post("/urls/:id", (req, res) => {
+  let longURL = req.body.longURL
+  urlDatabase[req.params.id] = longURL;
+  res.redirect("/urls")
+  });
+
+
+
+  // console.log(req.body);  // debug statement to see POST parameters
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
